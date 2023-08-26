c117d80f4e21b1ab8498da648a796d9e35b4b922
RobssJR <robson.alex.jr@gmail.com>
2023-08-26 18:07:31 -0300

    Ajustando reconhecimento

--- Camera/main.py
+++ Camera/main.py
@@ -1,4 +1,5 @@
 import pickle
+
 import cv2
 import face_recognition
 
@@ -17,14 +18,12 @@ while True:
     face_locations = face_recognition.face_locations(rgb_frame)
     face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
-    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+    for face_encoding in face_encodings:
         label_id = clf.predict([face_encoding])
         person_name = label_encoder.inverse_transform(label_id)[0]
 
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, person_name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
-
+        print("Pessoa reconhecida:", person_name)
+        
     cv2.imshow('Video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
